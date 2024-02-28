cd778ed8ca9f50f8be95ea04bc6d528a86991cf5
samuel calderon <samuelcalderon4343@gmail.com>
2024-02-28 13:14:06 +0100

    cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
