7aa51225fae314d54c54d125c9c8dbcfef5b4dc5
oreterraC <diegolanseiros@gmail.com>
2024-03-06 17:31:50 +0100

    Empiezo con las pruebas de nivel, tengo que mirar como configurar el app para que no se quede escuchando indefinidamente

--- test/components/application.test.js
+++ test/components/application.test.js
@@ -2,13 +2,29 @@ import request from "supertest";
 import app from "../../app";
 
 describe("Category Component", () => {
-  let category;
-
   describe("GET /api/level/categories", () => {
     test("should return all categories", async () => {
       const response = await request(app).get("/api/level/categories");
       expect(response.statusCode).toBe(200);
       expect(response.body.length).toBe(7);
+    });
+  });
+});
+
+describe("Level Component", () => {
+  describe("GET /api/level/all", () => {
+    test("should return all levels", async () => {
+      const response = await request(app).get("/api/level/all");
+      expect(response.statusCode).toBe(200);
+      expect(response.body.length).not.toBe(null);
+    });
+  });
+
+  describe("GET /api/level/all", () => {
+    test("should return all levels", async () => {
+      const response = await request(app).get("/api/level/all");
+      expect(response.statusCode).toBe(200);
+      expect(response.body.length).not.toBe(null);
     });
   });
 });
