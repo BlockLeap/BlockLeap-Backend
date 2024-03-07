0ca345d349b5d45e4da35b6907d6194d66362435
oreterraC <diegolanseiros@gmail.com>
2024-03-07 18:42:19 +0100

    Pruebas de nivel terminadas a excepción del POST create, tengo que preguntar

--- test/components/application.test.js
+++ test/components/application.test.js
@@ -20,9 +20,33 @@ describe("Level Component", () => {
     });
   });
 
-  describe("GET /api/level/all", () => {
-    test("should return all levels", async () => {
-      const response = await request(app).get("/api/level/all");
+  describe("GET /api/level/:id", () => {
+    let id = 1;
+    test("should return one specific level", async () => {
+      const response = await request(app).get(`/api/level/${id}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toBe(1);
+      expect(response.body.user).toBe(1);
+      expect(response.body.category).toBe(1);
+      expect(response.body.self).toBe(1);
+      expect(response.body.title).toBe("Movimiento");
+    });
+  });
+
+  describe("GET /api/level/levelsByCategory/:id", () => {
+    let id = 1;
+    test("should return all levels of a specific category", async () => {
+      const response = await request(app).get(
+        `/api/level/levelsByCategory/${id}`
+      );
+      expect(response.statusCode).toBe(200);
+      expect(response.body.length).not.toBe(null);
+    });
+  });
+
+  describe("GET /api/level/community/levels", () => {
+    test("should return only community levels", async () => {
+      const response = await request(app).get("/api/level/community/levels");
       expect(response.statusCode).toBe(200);
       expect(response.body.length).not.toBe(null);
     });
