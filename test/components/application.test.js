8f1102332624427c09e2a6ae1b8edd1f36e57f1c
oreterraC <diegolanseiros@gmail.com>
2024-03-09 19:14:19 +0100

    Continúo con las pruebas de la entidad grupo

--- test/components/application.test.js
+++ test/components/application.test.js
@@ -16,6 +16,7 @@ describe("Level Component", () => {
     test("should return all levels", async () => {
       const response = await request(app).get("/api/level/all");
       expect(response.statusCode).toBe(200);
+      // Buscar una mejor comprobación
       expect(response.body.length).not.toBe(null);
     });
   });
@@ -40,6 +41,7 @@ describe("Level Component", () => {
         `/api/level/levelsByCategory/${id}`
       );
       expect(response.statusCode).toBe(200);
+      // Buscar una mejor comprobación
       expect(response.body.length).not.toBe(null);
     });
   });
@@ -48,6 +50,42 @@ describe("Level Component", () => {
     test("should return only community levels", async () => {
       const response = await request(app).get("/api/level/community/levels");
       expect(response.statusCode).toBe(200);
+      // Buscar una mejor comprobación
+      expect(response.body.length).not.toBe(null);
+    });
+  });
+});
+
+/* describe("User Component", () => {
+  describe("GET /api/user/userById/:id", () => {
+    let id = 1;
+    test("should return one specific user", async () => {
+      const response = await request(app).get(`/api/user/userById/${id}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toBe(1);
+      expect(response.body.name).toBe("Diego");
+      expect(response.body.role).toBe("Estudiante");
+    });
+  });
+}); */
+
+describe("Group Component", () => {
+  describe("GET /api/group/:groupId", () => {
+    let id = 15;
+    test("should return one specific group", async () => {
+      const response = await request(app).get(`/api/group/${id}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toBe(15);
+      expect(response.body.name).toBe("Grupo 5");
+    });
+  });
+
+  describe("GET /api/group/:groupId/members", () => {
+    let id = 15;
+    test("should all members of a specific group", async () => {
+      const response = await request(app).get(`/api/group/${id}/members`);
+      expect(response.statusCode).toBe(200);
+      // Buscar una mejor comprobación
       expect(response.body.length).not.toBe(null);
     });
   });
